cf996f0f994d7cff96b95863dd7b7818731b667a
samps0 <ssmith@imediainc.com>
2022-02-09 16:26:01 -0600

    Improving layout on smaller devices

--- js/script.js
+++ js/script.js
@@ -4,13 +4,43 @@ function addRailEventListeners() {
     console.log('#' + item.id + ' + .package-details-dropdown-rail')
     document.querySelector('#'+item.id + ' + .package-details-dropdown-rail').addEventListener('click', event => {
       if(!item.classList.contains('open')) {
-        item.classList.add('opening');
+        item.classList.remove('close');
+        item.classList.add('open');
+        item.querySelector('.dropdown-topline').style.visibility = "visible";
+      } else {
+        item.classList.add('close');
+        item.classList.remove('open');
+        item.querySelector('.dropdown-topline').style.visibility = "hidden";
       }
-      //item.classList.add('closing');
     })
+  })
+}
+
+
+function addJSBreakpoints() {
+  const windowWidth = window.innerWidth;
+
+  if(windowWidth > 1700) {
+
+  } else if (windowWidth > 1500) {
+      
+  } else if (windowWidth > 1200) {
+
+  } else if (windowWidth > 800) {
+
+  } else {
+
+  }
+}
+
+function addResizeEventListeners() {
+  window.addEventListener('resize', function () {
+    addJSBreakpoints();
   })
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
     addRailEventListeners();
+    addJSBreakpoints();
+    addResizeEventListeners();
 });
